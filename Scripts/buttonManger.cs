2ee05719ce4d55eb0247b22cb3e3ae3d45a5e2f4
mountrivers <36880919+mountrivers@users.noreply.github.com>
2018-03-09 14:29:56 +0900

    vr pointing code and nonvr code
    buttonManger - if gaizing something, the thins invoked by gvralleventtrigers which addlistner will wait for invoked
    and clasify the gazing method's name, and actvie 
    nonvr - if don't need VR Off the VR mode

---
+++ Scripts/buttonManger.cs
@@ -0,0 +1,83 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class buttonManger : GvrAllEventsTrigger
+{
+
+    public GameObject Uis;
+    int ist;
+    string tname;
+    string outname;
+    // Use this for initialization
+    void Start()
+    {
+        HideCube2();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnPointerEnter.AddListener(Getingtarget);
+        OnPointerUp.AddListener(Getingtargetu);
+        OnPointerExit.AddListener(Getingtargeto);
+    }
+    public void Getingtarget(GameObject target, PointerEventData eventData)
+    {
+        if (target)
+            tname = target.name;
+    }
+    public void Getingtargetu(GameObject target, PointerEventData eventData)
+    {
+        if (target)
+            tname = target.name;
+    }
+
+    public void Getingtargeto(GameObject target, PointerEventData eventData)
+    {
+        if (target)
+            outname = target.name;
+    }
+
+    public void ShowCube2()
+    {
+        print(tname);
+        if (tname == "Cube")
+        {
+            print("hell");
+            if (Uis.activeInHierarchy == true)
+            {
+                Uis.SetActive(false);
+            }
+            else
+            {
+                Uis.SetActive(true);
+            }
+        }
+        if (tname == "Cubes")
+        {
+
+            StartCoroutine(DoWait());
+
+        }
+    }
+
+    public void HideCube2()
+    {
+        Uis.SetActive(false);
+    }
+    IEnumerator DoWait()
+    {
+        outname = "0";
+        yield return new WaitForSeconds(3);
+        if (outname == "0")
+            SceneManager.LoadScene("k");
+        print(outname);
+        
+
+    }
+
+}
