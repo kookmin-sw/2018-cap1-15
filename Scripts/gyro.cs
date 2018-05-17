86392e3c35d11b3e6e1797d1076990148c38676d
mountrivers <36880919+mountrivers@users.noreply.github.com>
2018-05-17 23:55:09 +0900

    gyro test 2

--- Scripts/gyro.cs
+++ Scripts/gyro.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class gyro : MonoBehaviour {
+    float _BaseAngle = -90f;
 
-    Vector3 gyroscope_rotation;
-    
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
     void Update()
     {
-        gyroscope_rotation.x += Input.gyro.rotationRateUnbiased.x;
-        gyroscope_rotation.y += Input.gyro.rotationRateUnbiased.y;
+        Quaternion q1 = Input.gyro.attitude;
+        Quaternion q2 = new Quaternion(q1.y, -q1.z, -q1.x, q1.w);
+
+        transform.rotation = Quaternion.Euler(new Vector3(q2.eulerAngles.x, _BaseAngle, q2.eulerAngles.z));
+        if (transform.rotation.eulerAngles.z <= 275f && transform.rotation.eulerAngles.z > 180f)
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 275f));
+        if (transform.rotation.eulerAngles.z >= 355f || transform.rotation.eulerAngles.z <= 180f)
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 355f));
     }
 
     public void gyroOn()
