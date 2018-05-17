5b2855a7c544ca1cd59dad05fd04401446d444ae
mountrivers <36880919+mountrivers@users.noreply.github.com>
2018-05-17 23:48:08 +0900

    gyro test

---
+++ Scripts/gyro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyro : MonoBehaviour {
+
+    Vector3 gyroscope_rotation;
+    
+    void Update()
+    {
+        gyroscope_rotation.x += Input.gyro.rotationRateUnbiased.x;
+        gyroscope_rotation.y += Input.gyro.rotationRateUnbiased.y;
+    }
+
+    public void gyroOn()
+    {
+        Input.gyro.enabled = true;
+    }
+    
+}
