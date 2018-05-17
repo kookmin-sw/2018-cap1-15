d2e42e626d8daf5c738df8f74c0bed0fc49abe9d
mountrivers <36880919+mountrivers@users.noreply.github.com>
2018-05-18 00:01:43 +0900

    gyro 관련 제약 추가 후 테스팅

--- Scripts/gyro.cs
+++ Scripts/gyro.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class gyro : MonoBehaviour {
     float _BaseAngle = -90f;
-
+    int gyroCoin = 0;
     void Start()
     {
-        Input.gyro.enabled = true;
     }
 
     void Update()
     {
-        Quaternion q1 = Input.gyro.attitude;
-        Quaternion q2 = new Quaternion(q1.y, -q1.z, -q1.x, q1.w);
+        if (gyroCoin ==1)
+        {
+            Quaternion q1 = Input.gyro.attitude;
+            Quaternion q2 = new Quaternion(q1.y, -q1.z, -q1.x, q1.w);
 
-        transform.rotation = Quaternion.Euler(new Vector3(q2.eulerAngles.x, _BaseAngle, q2.eulerAngles.z));
-        if (transform.rotation.eulerAngles.z <= 275f && transform.rotation.eulerAngles.z > 180f)
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 275f));
-        if (transform.rotation.eulerAngles.z >= 355f || transform.rotation.eulerAngles.z <= 180f)
-            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 355f));
+            transform.rotation = Quaternion.Euler(new Vector3(q2.eulerAngles.x, _BaseAngle, q2.eulerAngles.z));
+            if (transform.rotation.eulerAngles.z <= 275f && transform.rotation.eulerAngles.z > 180f)
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 275f));
+            if (transform.rotation.eulerAngles.z >= 355f || transform.rotation.eulerAngles.z <= 180f)
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 355f));
+        }
     }
 
     public void gyroOn()
     {
         Input.gyro.enabled = true;
+        gyroCoin = 1;
+    }
+    public void gyroOff()
+    {
+        Input.gyro.enabled = false;
+        gyroCoin = 0;
     }
     
 }
